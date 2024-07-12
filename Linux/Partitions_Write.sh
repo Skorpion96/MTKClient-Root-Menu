6683b6d5c3a9460add9d1206fa8b4f6bae8ebe23
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2024-07-12 18:55:37 +0200

    Add files via upload

---
+++ Linux/Partitions_Write.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Prompt the user for input
+echo -n "Please enter the partitions you want to write (separated by spaces): "
+
+# Read the input partitions
+read -r -a partitions_array
+
+# Check if any partitions were provided
+if [ ${#partitions_array[@]} -eq 0 ]; then
+    echo "No partitions provided. Exiting."
+    exit 1
+fi
+
+# Construct the command to flash all partitions
+partitions_list=""
+img_list=""
+for ((i = 0; i < ${#partitions_array[@]}; i++)); do
+    partition="${partitions_array[i]}"
+    modified_partition="${partition}.img"
+    if [ $i -eq 0 ]; then
+        partitions_list="$partition"
+        img_list="$modified_partition"
+    else
+        partitions_list+=",$partition"
+        img_list+=",$modified_partition"
+    fi
+done
+
+# Construct the final flash command
+flash_command="python3 mtk.py w $partitions_list $img_list"
+
+# Execute the command to flash all partitions
+echo "Executing command: $flash_command"
+# Uncomment the following line to actually run the command
+ eval "$flash_command"
+
+
+
+
+
+
